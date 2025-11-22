ea7f35cf0a6156e28d91f7bd22d1239c94a28599
AyushKirtania-tech <142684074+AyushKirtania-tech@users.noreply.github.com>
2025-11-23 00:45:08 +0530

    testing

--- backend/src/routes/customer.router.ts
+++ backend/src/routes/customer.router.ts
@@ -3,6 +3,10 @@ import { Router, Request, Response } from "express";
 const JWT_SECRET = process.env.JWT_SECRET || "";
 
 const router = Router();
-
+router.get("/login",(req:Request,res:Response)=>{
+    res.json({
+        message:"Login Succesful"
+    })
+})
 
 export default router;
